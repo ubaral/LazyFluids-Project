e962341c3671f7998a8740d3bb90dfa6ab16a018
Utsav Baral <utsavbaral@gmail.com>
2017-03-10 01:58:22 -0800

    image update logic, yet to implement nearest-neighbors

--- main.py
+++ main.py
@@ -1,66 +1,83 @@
-'''
-Title: Lazy Fluids Project
-File name: main.py
-Author: Utsav Baral
-Date created: 3/6/2017
-Python Version: 3.5
-'''
-
 import numpy as np
 import skimage as sk
 import skimage.io as skio
 from skimage.transform import rescale
 from scipy import ndimage
 import random
+import scipy.sparse
+import scipy.sparse.linalg
 
 
 # Function that takes in an image, a pixel within the image, and a nbhd width. Returns an array containing the nbhd of
 # pixels
 def get_nbhd(img, pix_coord, width):
+    # pixel_index = patch[0] * out.shape[1] + patch[1]
     pass
 
-# Function that takes in two patches of pixels and returns the error between the two pixels
-def calculate_error(patch1, patch2):
+
+# For all patches in X, find the patch in Z that is the nearest neighbor, and return the mapping
+def nearest_neighbors(X, Z):
     pass
 
 # name of the input file
 imname = 'texture.jpg'
-
 # read in the image
 Z_src = skio.imread(imname)
-
 nbhd_width = 32
-
 # output image X, size is 800 by 800
-out = np.ndarray((800,800,3))
+out = np.ndarray((400, 400, 3))
 
 # Create the set of points that are the centers of the neighborhoods we will be comparing
-x_centers = np.arange(nbhd_width/2, out.shape[0] - nbhd_width/2, nbhd_width/4)
-y_centers = np.arange(nbhd_width/2, out.shape[1] - nbhd_width/2, nbhd_width/4)
+x_centers = np.arange(nbhd_width / 2, out.shape[0] - nbhd_width / 2, nbhd_width / 4)
+y_centers = np.arange(nbhd_width / 2, out.shape[1] - nbhd_width / 2, nbhd_width / 4)
 
 # Cartesian product of the x and y partitions
 prod = np.transpose([np.tile(x_centers, len(y_centers)), np.repeat(y_centers, len(x_centers))])
 nbhd_centers = set([tuple(prod[i]) for i in range(len(prod))])
 
-output_patch_nearest_neigbors = dict.fromkeys(nbhd_centers)
+# Map from output patches to input patches that are the nearest neighbors
+current_neighbors = dict.fromkeys(nbhd_centers)
 
 # Initialize the nearest neighbors for each nbhd in X(output) to random nbhds in Z(source)
-for patch in output_patch_nearest_neigbors:
+for patch in current_neighbors:
     # x_rand = random.randrange(0, Z_src.shape[0])
     # y_rand = random.randrange(0, Z_src.shape[1])
-    x_rand = random.randrange(nbhd_width/2, Z_src.shape[0] - nbhd_width/2 + 1)
-    y_rand = random.randrange(nbhd_width/2, Z_src.shape[1] - nbhd_width/2 + 1)
-    output_patch_nearest_neigbors[patch] = (x_rand, y_rand)
+    x_rand = random.randrange(nbhd_width / 2, Z_src.shape[0] - nbhd_width / 2 + 1)
+    y_rand = random.randrange(nbhd_width / 2, Z_src.shape[1] - nbhd_width / 2 + 1)
+    current_neighbors[patch] = (x_rand, y_rand)
 
-N = 10 # number of iterations
+N = 1  # number of iterations
 for i in range(N):
-    # update the output image to be the pixel values that minimize the Energy Function. Should calculate as a system of
-    # linear equations, Ax = b. Removed previous energy for
-    pass
+    diags = np.zeros(out[:, :, 0].size)  # just the diagonals of the matrix A
+    b = np.zeros(out[:, :, 0].size)  # vector b on the RHS of matrix equation Ax = b
+
+    # For each output patch, loop through each pixel in the input patch and NN-output patch, and increment the entry
+    # along the diagonal of matrix A corresponding to the output patch pixel's index in the full size output image
+    # vector. At the same time, for the corresponding nearest neighbor output patch pixel, put that pixel value in the
+    # same index in the b vector.
+    for patch in current_neighbors:
+        # indices contains a list of integers that are the diagonals to update in the matrix A
+        # aka indices of neighborhood pixels in the flattened image
+        indices = get_nbhd(out, patch, out.shape[1])
+        z_pixels = Z_src[:, :, 0].flatten()[get_nbhd(Z_src, current_neighbors[patch], Z_src.shape[1])]
+
+        # update the b vector
+        b[indices] += z_pixels
+        # increment the diagonals of A
+        diags[indices] += 1
+
+    updated_image = scipy.sparse.linalg.solve_triangular(scipy.sparse.diags(diags), b)
+
+    # Follow rest of algorithm (1) in the paper:
+    # Compute the nearest neighbors of updated_image
+    # Maybe use some efficient library for the nearest neighbors computation?
+    updated_neighbors = nearest_neighbors(updated_image, Z_src)
+    # If updated neighbors is the same as current neighbors, then exit the loop and set the output image to the
+    # updated image
 
 # save the image
-#fname = 'output.jpg'
-#skio.imsave(fname, im)
+# fname = 'output.jpg'
+# skio.imsave(fname, im)
 
 # display the image
 skio.imshow(Z_src)
